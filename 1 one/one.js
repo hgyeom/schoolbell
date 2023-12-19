a1138a135a1bd01b625885c18b50908fbae5478a
hgyeom <rrune3@naver.com>
2023-12-19 17:35:42 +0900

    1번, 2번 수정

--- 1 one/one.js
+++ 1 one/one.js
@@ -1,32 +1,73 @@
-function getMaxNumber() {
-  // 정수형끼리의 덧셈이 되지 않도록 문자열로 저장
-  const nums = ['9', '7', '5', '3', '1'];
-  let num1 = '';
-  let num2 = '';
+// function getMaxNumber() {
+//   // 정수형끼리의 덧셈이 되지 않도록 문자열로 저장
+//   const nums = ['9', '7', '5', '3', '1'];
+//   let num1 = '';
+//   let num2 = '';
 
-  // 가장 큰 수 두개를 곱해야 가장 큰 수가 나온다.
-  // 가장 큰 수를 여러번 곱할수록 큰 수가 나온다.
+//   // 가장 큰 수 두개를 곱해야 가장 큰 수가 나온다.
+//   // 가장 큰 수를 여러번 곱할수록 큰 수가 나온다.
 
-  // 5개의 수를 조합하여 큰 수를 만드려면 3자리 * 2자리
-  // 그럼, 가장 큰 수를 여러번 곱하기 위해 2자리의 십의자리에 넣는다. ( 3번 곱할 수 있다.)
-  num2 += nums[0];
+//   // 5개의 수를 조합하여 큰 수를 만드려면 3자리 * 2자리
+//   // 그럼, 가장 큰 수를 여러번 곱하기 위해 2자리의 십의자리에 넣는다. ( 3번 곱할 수 있다.)
+//   num2 += nums[0];
 
-  // 가장 큰 수와 다음 큰 수를 곱해야 하기 때문에 3자리의 백의자리에 넣는다.
-  num1 += nums[1];
+//   // 가장 큰 수와 다음 큰 수를 곱해야 하기 때문에 3자리의 백의자리에 넣는다.
+//   num1 += nums[1];
 
-  // 다음으로, 가장 큰 수와 3번째로 큰 수를 곱해야 한다. 3자리의 십의자리에 3번째로 큰 수가 들어가야 한다.
-  // 이제 4번째로 큰 수와 마지막 수만 남았다. 4번째로 큰 수를 더 많이 곱해야 하니 2자리의 빈 자리에 넣는다.
-  // 자연스럽게 마지막 수는 3자리의 빈 자리에 넣는다.
-  for (let i = 2; i < nums.length; i++) {
-    if (i % 2 === 0) {
-      num1 += nums[i];
-    } else {
-      num2 += nums[i];
+//   // 다음으로, 가장 큰 수와 3번째로 큰 수를 곱해야 한다. 3자리의 십의자리에 3번째로 큰 수가 들어가야 한다.
+//   // 이제 4번째로 큰 수와 마지막 수만 남았다. 4번째로 큰 수를 더 많이 곱해야 하니 2자리의 빈 자리에 넣는다.
+//   // 자연스럽게 마지막 수는 3자리의 빈 자리에 넣는다.
+//   for (let i = 2; i < nums.length; i++) {
+//     if (i % 2 === 0) {
+//       num1 += nums[i];
+//     } else {
+//       num2 += nums[i];
+//     }
+//   }
+
+//   console.log(num1, num2);
+//   return parseInt(num1) * parseInt(num2);
+// }
+
+// console.log(getMaxNumber());
+
+function solution(numbers) {
+  let max = 0;
+  let temp = [];
+  const result = [];
+  const N = numbers.length;
+  const visited = new Array(N).fill(false);
+
+  const dfs = (n, str) => {
+    if (n === N) {
+      result.push(str);
+      return;
     }
-  }
 
-  console.log(num1, num2);
-  return parseInt(num1) * parseInt(num2);
+    for (let i = 0; i < N; i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        dfs(n + 1, str + numbers[i]);
+        visited[i] = false;
+      }
+    }
+  };
+
+  dfs(0, '');
+
+  result.forEach((strNum) => {
+    for (let i = 1; i < strNum.length; i++) {
+      const first = parseInt(strNum.slice(0, i));
+      const second = parseInt(strNum.slice(i));
+      if (max < first * second) {
+        max = first * second;
+        temp = `${first}, ${second}`;
+      }
+    }
+  });
+
+  return [max, temp];
 }
 
-console.log(getMaxNumber());
+const arr = [1, 3, 5, 7, 9];
+console.log(arr, solution(arr));
--- 2 two/two.js
+++ 2 two/two.js
@@ -1,48 +1,111 @@
 // 여기에 정답을 작성해주세요
 
+function $(name) {
+  if (!(this instanceof $)) {
+    return new $(name);
+  }
+
+  this.target = document.querySelector(name);
+
+  this.removeClass = function (className) {
+    if (this.target) {
+      this.target.classList.remove(className);
+    }
+    return this;
+  };
+
+  this.addClass = function (className) {
+    if (this.target) {
+      this.target.classList.add(className);
+    }
+    return this;
+  };
+
+  this.css = function (property, value) {
+    if (this.target) {
+      if (typeof property === 'string') {
+        console.log('문자열', property);
+        // 단일 프로퍼티 설정
+        this.target.style[property] = value;
+      } else if (typeof property === 'object') {
+        // 여러 프로퍼티 설정
+        console.log('객체', property);
+        for (var key in property) {
+          if (property.hasOwnProperty(key)) {
+            this.target.style[key] = property[key];
+          }
+        }
+      }
+    }
+    return this;
+  };
+
+  this.fadeOut = function (callback) {
+    if (this.target) {
+      let opacity = 1;
+      let fadeOutInterval = setInterval(
+        function () {
+          if (opacity > 0) {
+            opacity -= 0.1;
+            this.target.style.opacity = opacity;
+          } else {
+            clearInterval(fadeOutInterval);
+            this.target.style.display = 'none';
+            if (callback) {
+              callback();
+            }
+          }
+        }.bind(this),
+        100
+      );
+    }
+    return this;
+  };
+}
+
 // 1
-const target1 = document.querySelector('#target-1');
-target1.classList.remove('border');
+// const target1 = document.querySelector('#target-1');
+// target1.classList.remove('border');
 
-// 2
-target1.style.left = '250px';
+// // 2
+// target1.style.left = '250px';
 
-// 3
-const target2 = document.querySelector('.target-2');
-target2.classList.remove('border');
-target2.classList.add('blue');
+// // 3
+// const target2 = document.querySelector('.target-2');
+// target2.classList.remove('border');
+// target2.classList.add('blue');
 
-// 3 replace
-// target2.classList.replace('border','blue');
+// // 3 replace
+// // target2.classList.replace('border','blue');
 
-// 4
-target2.style.cssText = 'left: 50px; margin-top: -15px;';
+// // 4
+// target2.style.cssText = 'left: 50px; margin-top: -15px;';
 
-// 5
-const target3 = document.querySelector('#target-3');
-const target4 = document.querySelector('#target-4');
+// // 5
+// const target3 = document.querySelector('#target-3');
+// const target4 = document.querySelector('#target-4');
 
-target3.style.transition = 'opacity 500ms';
-target3.style.opacity = '0';
+// target3.style.transition = 'opacity 500ms';
+// target3.style.opacity = '0';
 
-target3.addEventListener('transitionend', () => {
-  target3.style.display = 'none';
-  target4.classList.add('green');
-});
+// target3.addEventListener('transitionend', () => {
+//   target3.style.display = 'none';
+//   target4.classList.add('green');
+// });
 
 // 아래 코드는 수정하지 않습니다
 
-// // 1
-// $('#target-1').removeClass('border');
+// 1
+$('#target-1').removeClass('border');
 
-// // 2
-// $('#target-1').css('left', '250px');
+// 2
+$('#target-1').css('left', '250px');
 
-// // 3
-// $('.target-2').removeClass('border').addClass('blue');
+// 3
+$('.target-2').removeClass('border').addClass('blue');
 
-// // 4
-// $('.target-2').css({ left: 50, 'margin-top': '-15px' });
+// 4
+$('.target-2').css({ left: 50, 'margin-top': '-15px' });
 
-// // 5
-// $('#target-3').fadeOut(() => $('#target-4').addClass('green'));
+// 5
+$('#target-3').fadeOut(() => $('#target-4').addClass('green'));
